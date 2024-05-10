72eb694b9feb7deb40399825a914eba44bbbc909
1etterh <1etterh.dev@gmail.com>
2024-05-10 09:24:10 +0900

    edit: CodeAnalyzer.py
    analzes code and returns dict of class, functions, variables, loop variables

--- CodeAnalyzer.py
+++ CodeAnalyzer.py
@@ -7,33 +7,55 @@ from collections import deque,defaultdict
 class CodeAnalyzer(ast.NodeTransformer):
     def __init__(self, code):
         self.ast = ast.parse(code)
-        self.variables = deque()
+        self.variables = defaultdict(lambda:None)
         self.functions = {}
         self.classes={}
-        base_dir = "graphs"
-        self.loopVar = defaultdict(lambda:None)
-        if not os.path.exists(base_dir):
-            os.makedirs(base_dir)
-        self.folder_name = os.path.join(base_dir, datetime.now().strftime("%Y%m%d_%H%M%S"))
-        os.makedirs(self.folder_name, exist_ok=True)
+        self.loop_variables = defaultdict(lambda:None)
+        
+
+        #making result graph dir
+        # base_dir = "graphs"
+        # if not os.path.exists(base_dir):
+        #     os.makedirs(base_dir)
+        # self.folder_name = os.path.join(base_dir, datetime.now().strftime("%Y%m%d_%H%M%S"))
+        # os.makedirs(self.folder_name, exist_ok=True)
 
     def visit_Assign(self,node):
         self.generic_visit(node)
         for target in node.targets:
             if isinstance(target,ast.Name):
-                self.variables.append(target.id)
+                self.variables[target.id]
         return node
     
     def visit_For(self, node):
         self.generic_visit(node)
         loop_var = astor.to_source(node.target).strip()
-        self.loopVar[loop_var]
+        self.loop_variables[loop_var]
         
+    def visit_FunctionDef(self, node):
+        self.generic_visit(node)
+        # Capture function definition in string format
+        function_code = astor.to_source(node)
+        self.functions[node.name] = function_code.strip()
+        return node
+    
+    def visit_ClassDef(self, node):
+        self.generic_visit(node)
+        # Capture class definition in string format
+        class_code = astor.to_source(node)
+        self.classes[node.name] = class_code.strip()
+        return node
+    
     def analyze(self):
         self.visit(self.ast)
 
+
 # Sample code for analysis
 code = '''
+class Math:
+    def add(self, x, y):
+        return x + y
+
 def compute(x, y):
     result = x + y
     return result
@@ -41,10 +63,12 @@ def compute(x, y):
 x = 0
 for i in range(5):
     compute(i, i + 1)
-    for j in range(i,10):
-        x+=j
+    x += i
 '''
+
 analyzer = CodeAnalyzer(code)
 analyzer.analyze()
-print(analyzer.variables)
-print(analyzer.loopVar)
+print("Variables:", *analyzer.variables)
+print("Loop Variables:",analyzer.loop_variables)
+print("Functions:",analyzer.functions)
+print("Classes:",analyzer.classes)
